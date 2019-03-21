a8803686289390b0370567fe70545ae4fafa2f10
Ignacio <ignaciosenese@gmail.com>
2019-03-22 08:20:13 +0900

    Grid working and rendering on the DOM

--- client/src/component/Component1.jsx
+++ client/src/component/Component1.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import $ from 'jquery';
 
 
 const Board = (props) => {
-
-
-
+  var buildBoard = function() {
+    for (var i = 0; i < 6; i++) {
+      $('.grid').append("<div class='row' id='row"+i+"'></div>");
+      for (var j = 0; j < 7; j++) {
+        $('#row'+i).append("<div class='square'></div>");
+      }
+    }
+  }
   return (
-    <div className="board">
-
-  <h1>Hello Board</h1>
-
-  </div>
-  );
-
+    buildBoard()
+  )
 };
 
 export default Board;
